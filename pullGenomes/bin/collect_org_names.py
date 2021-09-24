3a1a05b622b5ff9e9ef2ae967f3d37bd435a1fed
Matt--Tenaya <mkozubov@tenayathera.com>
2021-09-24 16:37:34 -0700

    Fixes regex to find genomes/name better

--- pullGenomes/bin/collect_org_names.py
+++ pullGenomes/bin/collect_org_names.py
@@ -10,6 +10,8 @@ rootDataDir = sys.argv[1]
 
 ncbi_accession = []
 organism_list = []
+ncbi2org = {}
+
 fastaExt = ['.fa', '.fna', '.faa', '.fasta']
 
 for filename in os.listdir(rootDataDir): # Loop over passed directory
@@ -17,24 +19,39 @@ for filename in os.listdir(rootDataDir): # Loop over passed directory
 
     if os.path.isdir(content): # If it is a directory
         ncbi_accession.append(filename)
+
         for filename2 in os.listdir(content): # Loop over the files inside
             content2 = os.path.join(content, filename2)
-            if (filename in filename2) and ('genom' in filename2) and (os.path.splitext(filename2)[1] in fastaExt): # if they are the genome file
+
+            if ( (filename in filename2) or ('unplaced' in filename2) or ('chr' in filename2) ) and (os.path.splitext(filename2)[1] in fastaExt): # if they are the genome file
                 with open(content2) as file:
                     handle = file.readlines()
+
                     for line in handle: # loop over genome file lines
                         if line[0] == '>': # if it is the header line
-                            org_exp = '\s.+,'
+                            org_exp = '\s.+[,|\n]' # Start wit ha space, end on a , or a newline
                             organism_name = re.search( org_exp, line )
+                            #print(filename, organism_name)
+                            ncbi2org[filename] = organism_name.group()[1:-1]
                             try:
                                 organism_list.append( organism_name.group()[1:-1] )
                             except AttributeError:
                                 organism_list.append( 'No_Name' )
                             break
+                break
+
+            #else:
+                #print(filename, filename2, os.path.splitext(filename2)[1], ( (filename in filename2) or ('unplaced' in filename2) ))
+
+#print( f'{len(ncbi_accession)} directories\n{len(organism_list)} Human names\n{len(ncbi2org)} Both' )
 
 with open('relations.txt', 'w') as file:
+    """
     for i in range(len(ncbi_accession)-1):
         file.write(f'{ncbi_accession[i]}\t{organism_list[i]}\n')
 
     finalIndex = len(ncbi_accession) - 1
     file.write(f'{ncbi_accession[finalIndex]}\t{organism_list[finalIndex]}')
+    """
+    for key, value in ncbi2org.items():
+        file.write(f'{key}\t{value}\n')
